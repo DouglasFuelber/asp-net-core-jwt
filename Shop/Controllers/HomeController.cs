7f2ec7af176bdd534608051596a6c26349d98f48
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-20 22:55:38 -0300

    Created authentication and authenticated routes

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using Shop.Repositories;
+using Shop.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "User or password invalid" });
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new { user, token };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anonymous";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Authenticated - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Employee";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Manager";
+
+    }
+}
--- Shop/Models/User.cs
+++ Shop/Models/User.cs
@@ -1,9 +1,12 @@
-﻿namespace Shop.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Shop.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        [JsonIgnore]
         public string Password { get; set; }
         public string Role { get; set; }
     }
